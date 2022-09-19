5bf38eb38556c6c8a9da5834a9eff5888f1ec931
Anki2002 <ankita.anki2006@gmail.com>
2022-09-19 21:28:44 +0530

    First commit

---
+++ src/test/java/com/example/jap1/Jap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
